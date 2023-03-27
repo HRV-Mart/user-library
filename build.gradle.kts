fe8671d750f402dd424664d347df46802694deba
Harsh3305 <harshverma3305@gmail.com>
2023-03-27 22:22:22 +0530

    Updated publisher

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ publishing {
 	repositories {
 		maven {
 			name = "GitHubPackages"
-			url = uri("https://maven.pkg.github.com/hrv-mart/product")
+			url = uri("https://maven.pkg.github.com/hrv-mart/user-library")
 			credentials {
 				username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
 				password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
