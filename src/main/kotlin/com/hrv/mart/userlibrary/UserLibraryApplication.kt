96af60b6ca7c75f3fb296f5ac2a9023faa08448c
Harsh3305 <harshverma3305@gmail.com>
2023-03-27 20:39:34 +0530

    First commit

---
+++ src/main/kotlin/com/hrv/mart/userlibrary/UserLibraryApplication.kt
@@ -0,0 +1,11 @@
+package com.hrv.mart.userlibrary
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UserLibraryApplication
+
+fun main(args: Array<String>) {
+	runApplication<UserLibraryApplication>(*args)
+}
---
+++ src/test/kotlin/com/hrv/mart/userlibrary/UserLibraryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hrv.mart.userlibrary
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserLibraryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
