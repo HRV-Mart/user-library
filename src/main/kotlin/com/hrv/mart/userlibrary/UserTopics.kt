012d003fca48905cffbbe65d8ab1dc71b1f17592
Harsh3305 <harshverma3305@gmail.com>
2023-03-28 20:49:20 +0530

    Created topic to delete user

--- src/main/kotlin/com/hrv/mart/userlibrary/UserTopics.kt
+++ src/main/kotlin/com/hrv/mart/userlibrary/UserTopics.kt
@@ -3,5 +3,6 @@ package com.hrv.mart.userlibrary
 class UserTopics {
     companion object {
         const val createUserTopic = "CREATE_USER_TOPIC"
+        const val deleteUserTopic = "DELETE_USER_TOPIC"
     }
 }
