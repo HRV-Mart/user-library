a141f83f6dc90f98d6e16ce3e8db83940941550d
Harsh3305 <harshverma3305@gmail.com>
2023-03-29 09:42:28 +0530

    Added reactive kafka support

--- src/main/kotlin/com/hrv/mart/userlibrary/UserProducer.kt
+++ src/main/kotlin/com/hrv/mart/userlibrary/UserProducer.kt
@@ -1,25 +1,24 @@
 package com.hrv.mart.userlibrary
 
-import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate
 import org.springframework.kafka.support.KafkaHeaders
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Service
 
 @Service
 class UserProducer (
-    private val kafkaTemplate: KafkaTemplate<String, User>
+    @Autowired
+    private val kafkaTemplate: ReactiveKafkaProducerTemplate<String, User>
 ) {
-    fun createUser(user: User) {
+    fun createUser(user: User) =
         sendMessage(user, UserTopics.createUserTopic)
-    }
-    fun deleteUser(userId: String) {
+    fun deleteUser(userId: String) =
         sendMessage(userId, UserTopics.deleteUserTopic)
-    }
-    private fun <T : Any> sendMessage(data: T, topic: String) {
-        val message = MessageBuilder
+    private fun <T : Any> sendMessage(data: T, topic: String) =
+        kafkaTemplate.send(topic, MessageBuilder
             .withPayload(data)
             .setHeader(KafkaHeaders.TOPIC, topic)
             .build()
-        kafkaTemplate.send(message)
-    }
+        )
 }
