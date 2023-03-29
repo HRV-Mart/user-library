1cf6b57b00933ebe02deeb351b394ab480bc1e38
Harsh3305 <harshverma3305@gmail.com>
2023-03-29 09:50:36 +0530

    change project structure

--- src/main/kotlin/com/hrv/mart/userlibrary/model/User.kt
+++ src/main/kotlin/com/hrv/mart/userlibrary/model/User.kt
@@ -1,4 +1,4 @@
-package com.hrv.mart.userlibrary
+package com.hrv.mart.userlibrary.model
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
